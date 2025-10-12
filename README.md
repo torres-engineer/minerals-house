649bbde27ba13f3688334d39b5c93b1cfa8041e6
João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>
2025-10-12 15:42:25 +0100

    chore: develop instructions and code format
    Signed-off-by: João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>

--- README.md
+++ README.md
@@ -1,18 +1,73 @@
+# Usage
+
+Install [Odin](https://odin-lang.org/docs/install/) and if you want to run it
+native, install
+[raylib](https://github.com/odin-lang/Odin/tree/master/vendor/raylib#build-and-installation)
+also.
+
+To run the native: `odin run ./native`
+
+To run the web version:
+
+```console
+./build.sh # or .\build.bat on Microsoft Windows
+# start a static web server on the web directory
+cd ./web
+python -m http.server
+php -S localhost:8000
+busybox httpd -f -p 8000
+```
+
 # Resources
 
 ## Odin
 
-https://odin-lang.org/
-https://github.com/odin-lang/examples/tree/master/raylib/
+https://odin-lang.org/ https://github.com/odin-lang/examples/tree/master/raylib/
 https://github.com/odin-lang/examples/tree/master/wasm/js_wasm32
 https://pkg.odin-lang.org/vendor/raylib/
 
 ## raylib
 
-https://www.raylib.com/
-https://www.raylib.com/examples.html
+https://www.raylib.com/ https://www.raylib.com/examples.html
 https://www.raylib.com/cheatsheet/cheatsheet.html
 
-## HTML `canvas` element
+## drawing graphics on the Web
 
 https://html.spec.whatwg.org/multipage/canvas.html
+https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Client-side_APIs/Drawing_graphics
+https://www.khronos.org/webgl/
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API
+https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/canvas
+
+## Storage and offline
+
+https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
+https://dexie.org/ https://rxdb.info/
+
+## a11y and performance
+
+https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Accessibility/What_is_accessibility
+https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Performance
+https://developer.mozilla.org/en-US/docs/Learn_web_development/Howto/Design_and_accessibility
+https://web.dev/learn/performance https://web.dev/learn/accessibility
+https://web.dev/learn/pwa https://codeamnesia.dev/ https://opendyslexic.org/
+https://gameaccessibilityguidelines.com/
+https://caniplaythat.com/category/resources/accessibility-reference-guides/
+
+## Testing
+
+https://www.selenium.dev/ https://www.w3.org/developers/tools/
+https://web.dev/learn/testing
+
+## Web components
+
+https://javascript.info/web-components
+
+## Others
+
+https://github.com/bradtraversy/design-resources-for-developers
+https://wikimediafoundation.org/ https://wikiba.se/
+https://www.wikidata.org/wiki/Wikidata:Main_Page
+https://commons.wikimedia.org/wiki/Main_Page https://www.go-fair.org/
+https://zenodo.org/
--- web/index.js
+++ web/index.js
@@ -3,96 +3,104 @@
 const PLAYER_POS_OFFSET = 0;
 
 (async () => {
-    const mem = new odin.WasmMemoryInterface();
-    const log = document.getElementById("console");
-    await odin.runWasm("index.wasm", log, null, mem);
-    const exports = mem.exports;
+  const mem = new odin.WasmMemoryInterface();
+  const log = document.getElementById("console");
+  await odin.runWasm("index.wasm", log, null, mem);
+  const exports = mem.exports;
 
-    const canvas = document.getElementById("game");
-    const ctx = canvas.getContext("2d");
+  const canvas = document.getElementById("game");
+  const ctx = canvas.getContext("2d");
 
-    exports.init(canvas.width, canvas.height);
-    const state = exports.getState();
+  exports.init(canvas.width, canvas.height);
+  const state = exports.getState();
 
-    let camera = {
-        target: mem.loadF32Array(state + PLAYER_POS_OFFSET, 2),
-        offset: [canvas.width / 2, canvas.height / 2],
-        rotation: 0,
-        zoom: 1
-    };
+  let camera = {
+    target: mem.loadF32Array(state + PLAYER_POS_OFFSET, 2),
+    offset: [canvas.width / 2, canvas.height / 2],
+    rotation: 0,
+    zoom: 1,
+  };
 
-    canvas.addEventListener("contextmenu", (e) => e.preventDefault());
-    canvas.addEventListener("mousedown", (e) => {
-        const pos = mem.loadF32Array(state + PLAYER_POS_OFFSET, 2);
-        if (e.button === 2) {
-            const worldX = pos[0] + (e.offsetX - camera.offset[0]);
-            const worldY = pos[1] + (e.offsetY - camera.offset[1]);
+  canvas.addEventListener("contextmenu", (e) => e.preventDefault());
+  canvas.addEventListener("mousedown", (e) => {
+    const pos = mem.loadF32Array(state + PLAYER_POS_OFFSET, 2);
+    if (e.button === 2) {
+      const worldX = pos[0] + (e.offsetX - camera.offset[0]);
+      const worldY = pos[1] + (e.offsetY - camera.offset[1]);
 
-            showInfo({ coords: [worldX, worldY] });
+      showInfo({ coords: [worldX, worldY] });
 
-            exports.player_click(worldX, worldY);
-        }
-
-        camera.target = mem.loadF32Array(state + PLAYER_POS_OFFSET, 2);
-    });
-
-    function screenToWorldX(x) {
-        return x - (camera.target[0] - camera.offset[0]);
-    }
-    function screenToWorldY(y) {
-        return y - (camera.target[1] - camera.offset[1]);
+      exports.player_click(worldX, worldY);
     }
 
-    function render() {
-        ctx.fillStyle = "#fefefe";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    camera.target = mem.loadF32Array(state + PLAYER_POS_OFFSET, 2);
+  });
 
-        const pos = mem.loadF32Array(state + PLAYER_POS_OFFSET, 2);
+  function screenToWorldX(x) {
+    return x - (camera.target[0] - camera.offset[0]);
+  }
+  function screenToWorldY(y) {
+    return y - (camera.target[1] - camera.offset[1]);
+  }
 
-        const gradient = ctx.createRadialGradient(
-            screenToWorldX(canvas.width / 2), screenToWorldY(canvas.height / 2),
-            0,
-            screenToWorldX(canvas.width / 2), screenToWorldY(canvas.height / 2),
-            canvas.height / 3
-        );
-        gradient.addColorStop(0, "green");
-        gradient.addColorStop(1, "skyblue");
-        ctx.fillStyle = gradient;
-        ctx.beginPath();
-        ctx.arc(screenToWorldX(canvas.width / 2), screenToWorldY(canvas.height / 2), canvas.height / 3, 0, Math.PI * 2);
-        ctx.fill();
+  function render() {
+    ctx.fillStyle = "#fefefe";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        ctx.fillStyle = "darkblue";
-        ctx.beginPath();
-        ctx.arc(
-            screenToWorldX(pos[0]),
-            screenToWorldY(pos[1]),
-            32,
-            0,
-            Math.PI * 2
-        );
-        ctx.fill();
+    const pos = mem.loadF32Array(state + PLAYER_POS_OFFSET, 2);
 
-        requestAnimationFrame(render);
-    }
+    const gradient = ctx.createRadialGradient(
+      screenToWorldX(canvas.width / 2),
+      screenToWorldY(canvas.height / 2),
+      0,
+      screenToWorldX(canvas.width / 2),
+      screenToWorldY(canvas.height / 2),
+      canvas.height / 3,
+    );
+    gradient.addColorStop(0, "green");
+    gradient.addColorStop(1, "skyblue");
+    ctx.fillStyle = gradient;
+    ctx.beginPath();
+    ctx.arc(
+      screenToWorldX(canvas.width / 2),
+      screenToWorldY(canvas.height / 2),
+      canvas.height / 3,
+      0,
+      Math.PI * 2,
+    );
+    ctx.fill();
+
+    ctx.fillStyle = "darkblue";
+    ctx.beginPath();
+    ctx.arc(
+      screenToWorldX(pos[0]),
+      screenToWorldY(pos[1]),
+      32,
+      0,
+      Math.PI * 2,
+    );
+    ctx.fill();
 
     requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
 })();
 
 function showInfo(info) {
-    const gameSection = document.querySelector("main>section:has(#game)");
+  const gameSection = document.querySelector("main>section:has(#game)");
 
-    const infoSection = document.createElement("section");
-    const sectionHeader = document.createElement("h2");
-    sectionHeader.innerText = "Information";
-    const article = document.createElement("article");
-    const articleHeader = document.createElement("h2");
-    articleHeader.innerText = "Object name";
-    const paragraph = document.createElement("p");
-    paragraph.innerText = JSON.stringify(info);
-    infoSection.append(sectionHeader, article);
-    article.append(articleHeader, paragraph);
+  const infoSection = document.createElement("section");
+  const sectionHeader = document.createElement("h2");
+  sectionHeader.innerText = "Information";
+  const article = document.createElement("article");
+  const articleHeader = document.createElement("h2");
+  articleHeader.innerText = "Object name";
+  const paragraph = document.createElement("p");
+  paragraph.innerText = JSON.stringify(info);
+  infoSection.append(sectionHeader, article);
+  article.append(articleHeader, paragraph);
 
-    gameSection.nextSibling.remove();
-    gameSection.insertAdjacentElement("afterend", infoSection);
+  gameSection.nextSibling.remove();
+  gameSection.insertAdjacentElement("afterend", infoSection);
 }
